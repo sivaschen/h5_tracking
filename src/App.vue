d5ebb50239664ecacf19759f10d89bb9879cecbd
sivaschen <crazychencheng@gmail.com>
2021-03-03 23:36:33 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <div class="banner">定位平台</div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -9,6 +10,10 @@
 </template>
 
 <style lang="less">
+html,body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,8 +23,10 @@
 }
 
 #nav {
-  padding: 30px;
-
+  .banner {
+    background-color: gray;
+    height: 80px;
+  }
   a {
     font-weight: bold;
     color: #2c3e50;
